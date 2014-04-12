285bbbcf3412cd911ad964f9ee556d409dabf844
Jed Borovik <mr.borovik@gmail.com>
2014-04-12 17:37:13 -0400

    Release 0.0.4

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 var methods = require('methods');
+var client = require('./lib/client');
+var server = require('./lib/server');
 
+exports.client = client;
+
+// Add server side routes
 methods.forEach(function(method) {
-  exports[method] = create(method);
+  exports[method] = server.create(method);
 });
 
 exports.del = exports.delete;
-exports.all = create();
-
-function create(method) {
-  if (method) method = method.toUpperCase();
-
-  return function(path, fn) {
-    var route = Object.create(null);
-
-    // intercept incoming server messages
-    route.server = function(primus) {
-      primus.transform('incoming', function(packet) {
-        var m = packet.data.method && packet.data.method.toUppercase();
-        var p = packet.data.path;
-
-        if (m === method && p === path) fn(packet);
-      });
-    }
-
-    return route;
-  }
-}
+exports.all = server.create();
